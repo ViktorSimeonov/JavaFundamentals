d97df1fd96dabb5f96ea998e2b26156d163ffa3e
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-09 15:13:21 +0200

    new task

--- src/BasicSyntaxConditionalStatementsAndLoops_Lab/MultiplicationTable2_11.java
+++ src/BasicSyntaxConditionalStatementsAndLoops_Lab/MultiplicationTable2_11.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class MultiplicationTable2_11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int m = Integer.parseInt(scanner.nextLine());
+        if (m > 10) {
+            System.out.printf("%d X %d = %d%n", n, m, (n * m));
+        }
+        for (int i = m; i <= 10; i++) {
 
+            System.out.printf("%d X %d = %d%n", n, i, (n * i));
+        }
     }
 }
