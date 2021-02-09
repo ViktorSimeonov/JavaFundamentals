ef991aa64a41a32b49839e578ae5096e0d836bd5
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-09 15:54:27 +0200

    new task

--- src/BasicSyntaxConditionalStatementsAndLoops_Lab/SumOfOddNumbers_09.java
+++ src/BasicSyntaxConditionalStatementsAndLoops_Lab/SumOfOddNumbers_09.java
@@ -7,16 +7,16 @@ public class SumOfOddNumbers_09 {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int sum = 0;
-        int count= 0;
-        int number =0;
-        while (count <n){
-
-            if (number%2!=0){
-            System.out.println(number);
+        int count = 0;
+        int number = 0;
+        while (count < n) {
+            if (number % 2 != 0) {
+                System.out.println(number);
                 count++;
-                       sum+=number;}
+                sum += number;
+            }
             number++;
         }
-        System.out.printf("Sum: %d",sum);
+        System.out.printf("Sum: %d", sum);
     }
 }
