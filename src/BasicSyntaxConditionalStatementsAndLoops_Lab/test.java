cabaae6afd10ca3c29f13ed9d75ea2ab05372f31
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-10 22:04:25 +0200

    new task

--- src/BasicSyntaxConditionalStatementsAndLoops_Lab/test.java
+++ src/BasicSyntaxConditionalStatementsAndLoops_Lab/test.java
@@ -1,12 +1,23 @@
 package BasicSyntaxConditionalStatementsAndLoops_Lab;
 import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
 
 public class test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 2000 ; i++) {
-            char a = (char)i;
-            System.out.printf("%c-%d%n",a,i);
-        }
+      //  Scanner scanner = new Scanner(System.in);
+        String input = "Geeks for Geeks";
+
+        StringBuilder input1 = new StringBuilder();
+
+        // append a string into StringBuilder input1
+        input1.append(input);
+
+        // reverse StringBuilder input1
+        input1.reverse();
+
+        // print reversed String
+        System.out.println(input1);
     }
 }
