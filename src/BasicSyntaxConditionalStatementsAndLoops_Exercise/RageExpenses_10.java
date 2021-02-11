d20d6f276b2d98f8e7310df844e90d9a6b1abf81
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-11 11:50:18 +0200

    new task

---
+++ src/BasicSyntaxConditionalStatementsAndLoops_Exercise/RageExpenses_10.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsAndLoops_Exercise;
+
+import java.util.Scanner;
+
+public class RageExpenses_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
