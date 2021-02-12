3bd0f1dc9ec6924b7a32e1f9ad6bd4a91ed7e10a
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-12 10:40:29 +0200

    new task

---
+++ src/ASCII.java
@@ -0,0 +1,34 @@
+
+public class ASCII {
+    public static void main(String[] args) {
+//        String input = "*";
+//        boolean valid = input.matches("\\w+");
+//        System.out.println(valid);
+        // char beginInterval = scanner.next().charAt(0); - четене на буква от конзолата
+//        for (int i = 0; i < 1000; i++) {
+//
+//            char a =(char)i;
+//            System.out.printf("%c %d%n",a,i);
+
+            char test = 97;
+            System.out.println(test);// резулатата е принтира 'а'
+            int testInt = 'a';// - за да вземем стойността на 'а'
+            System.out.println(testInt); //- резулатата е интеджер 97
+
+            // char symbol = 'a' + 2;
+            //това е равно на 'c" ! a = 97+2=99 в ASCII таблицата
+
+//            char symbol = 99; // този ред е равен на долните 2 т.е. резултата е 'c;
+//            int test = 'a'+2; // тук сбора е 99 долния ред го преобразува е char
+//            String fourthSymbol = Character.toString((char) test);
+            //char a =(char)99;// този ред също дава резултат 'с' преобразува число в буква ;)
+//            String str = "Welcome to string handling tutorial";
+//            //This will return the first char of the string
+//            char ch1 = str.charAt(0);
+//
+//            //This will return the 6th char of the string
+//            char ch2 = str.charAt(5);
+//        }
+
+    }
+}
