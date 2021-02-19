c4325c61af614b49414f87289adb3568c003d49d
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-19 10:17:14 +0200

    task Arrays

---
+++ src/Arrays_Lab/_1_DayOfWeek.java
@@ -0,0 +1,17 @@
+package Arrays_Lab;
+
+import java.util.Scanner;
+
+public class _1_DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int currentDay = Integer.parseInt(scanner.nextLine());
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday"};
+        if (currentDay >=1&&currentDay<=7) {
+            System.out.printf("%s", daysOfWeek[currentDay - 1]);
+        }else {
+            System.out.println("Invalid day!");
+        }
+    }
+}
