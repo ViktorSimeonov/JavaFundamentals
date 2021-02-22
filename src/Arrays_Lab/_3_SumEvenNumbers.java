507d97b0714a46ce67722e9afaa1ca851fa7eb1d
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-22 09:14:48 +0200

    new task

---
+++ src/Arrays_Lab/_3_SumEvenNumbers.java
@@ -0,0 +1,20 @@
+package Arrays_Lab;
+
+import java.util.Scanner;
+
+public class _3_SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numbers = scanner.nextLine();
+        String[] splitedArr = numbers.split(" ");
+        int[] arr = new int[splitedArr.length];
+        int sum=0;
+        for (int i = 0; i < splitedArr.length; i++) {
+            arr[i] = Integer.parseInt(splitedArr[i]);
+            if (arr[i] % 2 == 0){
+                sum+=arr[i];
+            }
+        }
+        System.out.print(sum);
+    }
+}
