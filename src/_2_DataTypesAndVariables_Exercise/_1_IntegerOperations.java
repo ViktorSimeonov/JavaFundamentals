aba9f184b61a6fbadaf3e1a94bcf159b11d7fb44
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-02-22 09:14:10 +0200

    new task

---
+++ src/_2_DataTypesAndVariables_Exercise/_1_IntegerOperations.java
@@ -0,0 +1,15 @@
+package _2_DataTypesAndVariables_Exercise;
+
+import java.util.Scanner;
+
+public class _1_IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 =Integer.parseInt(scanner.nextLine());
+        int num2 =Integer.parseInt(scanner.nextLine());
+        int num3 =Integer.parseInt(scanner.nextLine());
+        int num4 =Integer.parseInt(scanner.nextLine());
+        int result = ((num1+num2)/num3)*num4;
+        System.out.println(result);
+    }
+}
