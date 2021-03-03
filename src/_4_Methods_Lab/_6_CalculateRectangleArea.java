5018203404fb90ee37134a88c3320749760454ad
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-03-03 21:33:00 +0200

    Methods

---
+++ src/_4_Methods_Lab/_6_CalculateRectangleArea.java
@@ -0,0 +1,19 @@
+package _4_Methods_Lab;
+
+import java.util.Scanner;
+
+public class _6_CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double width = Double.parseDouble(scanner.nextLine());
+        double length = Double.parseDouble(scanner.nextLine());
+
+        double area = getRectangleArea(width, length);
+        System.out.printf("%.0f",area);
+    }
+
+    private static double getRectangleArea(double width, double length) {
+        double rectangleArea = width*length;
+        return rectangleArea;
+    }
+}
