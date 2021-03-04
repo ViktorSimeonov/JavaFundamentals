761261fb0a2370d7e33fb6a945b861b1f24eb059
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-03-04 09:09:54 +0200

    Methods

--- src/_4_Methods_Lab/_4_Calculations.java
+++ src/_4_Methods_Lab/_4_Calculations.java
@@ -1,4 +1,43 @@
 package _4_Methods_Lab;
 
+import java.util.Scanner;
+
 public class _4_Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String action = scanner.nextLine();
+        int x = Integer.parseInt(scanner.nextLine());
+        int y = Integer.parseInt(scanner.nextLine());
+        switch (action){
+            case "add":
+                add(x, y);
+                break;
+            case "multiply":
+                multiply(x, y);
+                break;
+            case "subtract":
+                substract(x, y);
+                break;
+            case "divide":
+                divide(x, y);
+                break;
+        }
+    }
+
+    public static void add(int x, int y){
+        int add = x+y;
+        System.out.println(add);
+    }
+
+    public static void multiply(int x, int y){
+        int multiply = x*y;
+        System.out.println(multiply);
+    }
+    private static void substract(int x, int y) {
+        int subtract = x-y;
+        System.out.println(subtract);
+    }private static void divide(int x, int y) {
+        int divide = x/y;
+        System.out.println(divide);
+    }
 }
