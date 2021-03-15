e52ea92435003307cb32b980ce6ace220af2d47c
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-03-15 12:01:52 +0200

    5.	List Manipulation Advanced Now we will implement more complicated list commands. Again, read a list, and until you receive "end" read commands: Contains {number} – check if the list contains the number. If yes print "Yes", otherwise print "No such number" Print even – print all the numbers that are even separated by a space Print odd – print all the numbers that are odd separated by a space Get sum – print the sum of all the numbers Filter ({condition} {number}) – print all the numbers that fulfill that condition. The condition will be either '<', '>', ">=", "<=" Example Input	Output 2 13 43 876 342 23 543 Contains 100 Contains 543 Print even Print odd Get sum Filter >= 43 Filter < 100 end	No such number Yes 2 876 342 13 43 23 543 1842 43 876 342 543 2 13 43 23

---
+++ src/_5_ListLab/_5_ListManipulationAdvanced.java
@@ -0,0 +1,161 @@
+package _5_ListLab;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class _5_ListManipulationAdvanced {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayList<Integer> numbers = createArrList(input);
+        String nextLine = scanner.nextLine();
+        // boolean end = false;
+        while (!"end".equals(nextLine)) {
+            String[] step = nextLine.split(" ");
+            String command = step[0];
+            int firstNum = 0;
+            int secondNum = 0;
+            String firstString = step[1];
+            switch (command) {
+                case "Contains":
+                    firstNum = Integer.parseInt(step[1]);
+                    if (numbers.contains(firstNum)) {
+                        System.out.println("Yes");
+                    } else {
+                        System.out.println("No such number");
+                    }
+                    break;
+                case "Print":
+                    if ("even".equals(firstString)) {
+                        printEven(numbers);
+                    }
+                    if ("odd".equals(firstString)) {
+                        printOdd(numbers);
+                    }
+                    //numbers.remove(Integer.valueOf(firstNum));
+                    break;
+                case "Get":
+                    if ("sum".equals(firstString)) {
+                        printSum(numbers);
+                    }
+                    break;
+                case "Filter":
+                    secondNum = Integer.parseInt(step[2]);
+                    if (firstString.length() == 1) {
+                        // '<' is 60, and '>' is 62
+                        char firstChar = firstString.charAt(0);
+                        // int valueOfChar = (int) firstChar;
+                        if ((int) firstChar == 60) {
+                            printLessThen(numbers, secondNum);
+                        } else if ((int) firstChar == 62) {
+                            printBiggerThen(numbers, secondNum);
+                        }
+                    } else if (firstString.length() == 2) {
+                        if ("<=".equals(firstString)) {
+                            printLessOrEqualTo(numbers, secondNum);
+                        } else if (">=".equals(firstString)) {
+                            printBiggerOrEqualTo(numbers, secondNum);
+                        }
+                    }
+                    break;
+
+            }
+
+            nextLine = scanner.nextLine();
+            /*if ("end".equals(nextLine)) {
+               // end = true;
+
+            }*/
+        }
+       /* if (end) {
+            printNumbers(numbers);
+        }*/
+
+    }
+
+    private static ArrayList<Integer> createArrList(String input) {
+        String[] arr = input.split(" ");
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+            int x = Integer.parseInt(s);
+            num.add(x);
+        }
+        return num;
+    }
+
+    private static void printNumbers(ArrayList<Integer> numbers) {
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printEven(ArrayList<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x % 2 == 0) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printOdd(ArrayList<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x % 2 == 1) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printSum(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
+        }
+        System.out.println(sum);
+    }
+
+    private static void printLessThen(ArrayList<Integer> numbers, int secondNum) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x < secondNum) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printBiggerThen(ArrayList<Integer> numbers, int secondNum) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x > secondNum) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printLessOrEqualTo(ArrayList<Integer> numbers, int secondNum) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x <= secondNum) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printBiggerOrEqualTo(ArrayList<Integer> numbers, int secondNum) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int x = numbers.get(i);
+            if (x >= secondNum) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+    }
+}
