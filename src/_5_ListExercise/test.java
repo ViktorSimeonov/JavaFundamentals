c6cd1e06914a86e9e8ee82cff372c92e49072a76
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-03-15 13:46:27 +0200

    task

---
+++ src/_5_ListExercise/test.java
@@ -0,0 +1,9 @@
+package _5_ListExercise;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
