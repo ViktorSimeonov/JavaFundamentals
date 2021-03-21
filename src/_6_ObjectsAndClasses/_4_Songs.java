fee502249062197c0ac93e247d9ae779042baf40
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-03-21 14:24:45 +0200

    4.	Songs Define a class Song, which holds the following information about songs: Type List, Name and Time. On the first line you will receive the number of songs - N. On the next N-lines you will be receiving data in the following format: "{typeList}_{name}_{time}". On the last line you will receive "Type List" / "all". Print only the Names of the songs which are from that Type List / All songs. Examples Input	Output 3 favourite_DownTown_3:14 favourite_Kiss_4:16 favourite_Smooth Criminal_4:01 favourite	DownTown Kiss Smooth Criminal 4 favourite_DownTown_3:14 listenLater_Andalouse_3:24 favourite_In To The Night_3:58 favourite_Live It Up_3:48 listenLater	Andalouse 2 like_Replay_3:15 ban_Photoshop_3:48 all	Replay Photoshop Solution Define class Song with fields: Type List, Name and Time:
    Define getters and setters: use keys ALT + INS and generate Getter and Setter:
    
    Read the input lines, make collection and store the data:
    
    Finally read your last line – Type List and print the result:
    
    You can use Stream API to filter the collection:

---
+++ src/_6_ObjectsAndClasses/_4_Songs.java
@@ -0,0 +1,8 @@
+package _6_ObjectsAndClasses;
+import java.util.Scanner;
+public class _4_Songs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
