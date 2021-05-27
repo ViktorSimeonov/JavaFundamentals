01a86b57769b0f976e3d42e879024b2fb5e678d3
ViktorSimeonov <77873986+ViktorSimeonov@users.noreply.github.com>
2021-05-27 23:34:59 +0300

    test

---
+++ src/BasicSyntaxConditionalStatementsAndLoops_Lab/_test.java
@@ -0,0 +1,15 @@
+package BasicSyntaxConditionalStatementsAndLoops_Lab;
+
+import java.util.Scanner;
+
+public class _test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "Pesho";
+        String secondName = scanner.nextLine();
+        System.out.println(name.equals(secondName));
+        double a = 5.25265464526;
+        int b = (int)5.56426543654365436;
+        System.out.println(b);
+    }
+}
